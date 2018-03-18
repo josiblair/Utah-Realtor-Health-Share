deb80fe0c41cf31e94f1a9b664462237a9a5c854
Josi Moore <josib.moore@gmail.com>
2018-03-18 14:57:50 -0600

    contact form set up and css

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,11 +1,74 @@
 import React, { Component } from 'react';
 import './ContactForm.css';
+import sign from '../../assets/sign.webp';
 
 class ContactForm extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    }
+  }
+
+updateName(name) {
+  this.setState({
+    name: name
+  })
+}
+
+updateEmail(email) {
+  this.setState({
+    email: email
+  })
+}
+
+updateSubject(subject) {
+  this.setState({
+    subject: subject
+  })
+}
+
+updateMessage(message) {
+  this.setState({
+    message: message
+  })
+}
+
+submitMessage(name, email, subject, message) {
+  //send email to Vaughn
+  console.log(name, email, subject, message);
+}
+
+
   render() {
+
+    const { name, email, subject, message } = this.state;
+
     return (
       <div className="ContactForm">
+        <div>
+          <span>ARE YOU READY TO TAKE CONTROL OF YOUR MEDICAL CARE COSTS?</span>
+          <span>Get A Free Quote!</span>
 
+          <span>vaughn@affiliateguard.info</span>
+          <span>801-688-4883</span>
+
+          <div>
+            <input type='text' value='Name*' onChange={ (e) => this.updateName(e.target.value) }/>
+            <input type='text' value='Email*' onChange={ (e) => this.updateEmail(e.target.value) }/>
+            <input type='text' value='Subject' onChange={ (e) => this.updateSubject(e.target.value) }/>
+            <input type='text' value='Message' onChange={ (e) => this.updateMessage(e.target.value) }/>
+
+            <button className='submit-button' onClick={ this.submitMessage(name, email, subject, message)} >Submit</button>
+          </div>
+        </div>
+        <div className='contact-pic'>
+          <img src={ sign } alt='sign' />
+        </div>
       </div>
     );
   }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Home.css';
 import logo from '../../assets/urhs-logo.png';
 import { Link } from 'react-router-dom';
+import ContactForm from '../ContactForm/ContactForm';
 
 import business from '../../assets/business1.webp';
 import calculate from '../../assets/calculate.webp';
@@ -16,45 +17,51 @@ class Home extends Component {
     return (
       <div className="Home">
         <div className="header">
-          <Link to='/'><img src={ logo } alt='logo' className='logo' /></Link>
-          <span>UTAH REALTOR HEALTH SHARE</span>
-
-          <div className='nav'>
-            <a href='#home'>Home</a>
-            <a href='#howitworks'>How It Works</a>
-            <Link to='/solution'><span>Solution</span></Link>
-            <a href='#contact'>Contact</a>
+          <div className='site-info'>
+            <Link to='/'><img src={logo} alt='logo' className='logo' /></Link>
+            <span className='site-name'>UTAH REALTOR HEALTH SHARE</span>
           </div>
 
-          <div className='phoneNumber'>801-688-4883</div>
+          <div className='nav-section'>
+            <div className='nav'>
+              <a href='#home' className='nav-link'>Home</a>
+              <a href='#howitworks' className='nav-link'>How It Works</a>
+              <Link to='/solution' className='nav-link'><span>The URHS Solution</span></Link>
+              <a href='#contact' className='nav-link'>Contact</a>
+            </div>
+
+            <div className='phoneNumber'>801-688-4883</div>
+          </div>
         </div>
 
         <div className='welcome'>
-          <span>INTRODUCING UTAH REALTOR HEALTH SHARE</span>
-          <span>Utah Realtor Health Share puts our clients’ needs first. By replacing traditional Major Medical Insurance with an Affordable Care Act Compliant Health Care Cost Sharing plan, we achieve immediate cost savings with long term rate stability.</span>
-          <Link to='/solution'><button className='learn-more'>Learn More</button></Link>
+          <div className='intro-container'>
+            <span className='intro'>INTRODUCING UTAH REALTOR HEALTH SHARE</span>
+            <span className='intro-paragraph'>Utah Realtor Health Share puts our clients’ needs first. By replacing traditional Major Medical Insurance with an Affordable Care Act Compliant Health Care Cost Sharing plan, we achieve immediate cost savings with long term rate stability.</span>
+            <Link to='/solution' className='learn-more'>Learn More</Link>
+          </div>
         </div>
 
         <div className='steps'>
-          <div className='step1'>
-            <img src={business} alt='business' className='business-img' />
-            <div>
+          <div className='step step1'>
+            <img src={business} alt='business' className='step-img business-img' />
+            <div className='step-info'>
               <span className='steps-header'>Obamacare Compliant</span>
               <span className='steps-paragraph'>As a member you are compliant with the individual mandate penalties under the ACA (Obamacare).</span>
             </div>
           </div>
 
-          <div className='step2'>
-            <div>
+          <div className='step step2'>
+            <div className='step-info'>
               <span className='steps-header'>Choose Your Doctor/Hospital</span>
               <span className='steps-paragraph'>One of the benefits of membership in a Health Care Cost Sharing organizaton is that you are not tied to a traditional network.</span>
             </div>
-            <img src={doctor} alt='doctor' className='doctor-img' />
+            <img src={doctor} alt='doctor' className='step-img doctor-img' />
           </div>
-          
-          <div className='step3'>
-          <img src={calculate} alt='calculate' className='calculate-img' />
-            <div>
+
+          <div className='step step3'>
+            <img src={calculate} alt='calculate' className='step-img calculate-img' />
+            <div className='step-info'>
               <span className='steps-header'>Typically 50% Less Expensive</span>
               <span className='steps-paragraph'>Monthly programs range from $235 per month for a single member under 30 years old to $857 per month for our most comprehensive program for a family which has no annual or lifetime maximums.</span>
             </div>
@@ -62,7 +69,7 @@ class Home extends Component {
         </div>
 
         <div className='how-it-works'>
-          <img src={ question } alt='faq' className='faq-icon' />
+          <img src={question} alt='faq' className='faq-icon' />
 
           <span>HOW IT WORKS</span>
           <span>Health Care Cost Sharing is an affordable alternative to expensive healthcare. Join a group of like minded individuals who share medical expenses together.</span>
@@ -71,13 +78,14 @@ class Home extends Component {
         </div>
 
         <div className='about'>
-          <img src={ meeting } alt='meeting' className='about-pic' />
+          <img src={meeting} alt='meeting' className='about-pic' />
 
           <span>A BIT ABOUT US</span>
           <span>Utah Realtor Health Share offers clients the best possible protection for all of their insurance needs. As one of the leading insurance companies in the greater San Francisco area, we have built quite the clientele by maintaining a strict customer-comes-first approach. If you are looking to do business with a top notch company that does everything in its power to ensure your satisfaction, contact us today. Our experts are here for your every insurance need and will be happy to supply you with a free estimate.</span>
         </div>
 
         <div className='contact'>
+          <ContactForm />
         </div>
       </div>
     );
