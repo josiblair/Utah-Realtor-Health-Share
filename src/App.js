6c0fb94f2e2c5a349c1ed5af9d0c19a3d185302a
Josi Moore <josib.moore@gmail.com>
2018-10-10 10:54:46 -0600

    set up server - getting ready to host

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Solution from './components/Solution/Solution';
+import Enroll from './components/Enroll/Enroll';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
           <Switch>
             <Route component={ Home } exact path='/' />
             <Route component={ Solution } path='/solution' />
+            <Route component={ Enroll } path='/enroll' />
           </Switch>
         </Router>
       </div>
